54e8521cf8c026bb32865e6b2378ba9c5b2006a2
Nikita Kozlov <kozlov_nikita@outlook.com>
2023-03-27 13:20:17 +0600

    chore core: npm updates & delete unusefull file

--- src/components/svg-render.js
+++
@@ -1,8 +0,0 @@
-export default function Image(props) {
-	const sourse = props.sourse || ''
-	const width = props.width ? props.width + 'rem' : '1.5rem'
-	const height = props.height ? props.height + 'rem' : '1.5rem'
-	const color = props.color || 'black'
-
-	return <img className="m-image--svg" src={sourse} color={color} />
-}
